b180bdf7ce9c7de833d6565d6b28b9522bceee6f
Rishi Sivaraju <sivarajurishi@gmail.com>
2025-05-01 20:54:21 +0530

    FrontEnd and Backend SETUP Complete

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config(); 
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json()); 
+
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express backend");
+});
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
